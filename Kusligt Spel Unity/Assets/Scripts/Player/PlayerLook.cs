3c64cabe02fbac1970e31313d778a2c73676baef
SchlyfeMormor <44261512+SchlyfeMormor@users.noreply.github.com>
2019-09-09 15:21:26 +0200

    Hej

---
+++ Kusligt Spel Unity/Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField] private string mouseXInput, mouseYInput;
+    [SerializeField] private float mouseSensitivity;
+
+    private void Awake()
+    {
+        LockCursor();
+    }
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxix(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxix(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        transform.Rotate(-transform.right * mouseY);
+    }
+}
+
+
